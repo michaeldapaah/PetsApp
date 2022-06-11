2b0e58f6466379cc67a2e2ab847628b80e158b88
michaeldapaah <michaeldapaah0@gmail.com>
2022-06-11 14:16:31 +0000

    collaboration

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:petsapp/screens/signup_screen.dart';
+import '../reusable_widgets/reusable_widget.dart';
+import '../utils/color_utils.dart';
+import 'home_screen.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _passwordTextController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+        hexStringToColor("CB2B93"),
+        hexStringToColor("9546C4"),
+        hexStringToColor("5E61F4")
+      ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+      child: SingleChildScrollView(
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                MediaQuery.of(context).size.width * 0.1,
+                MediaQuery.of(context).size.height * 0.1,
+                MediaQuery.of(context).size.width * 0.1,
+                150),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/pet.png"),
+                const SizedBox(
+                  height: 30,
+                ),
+                resuableTextField("Enter UserName", Icons.person_outline, false,
+                    _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                resuableTextField("Enter Password", Icons.lock_outline, true,
+                    _passwordTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                signInSignUpButton(context, true, () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomeScreen(),
+                      ));
+                }),
+                signUpOption()
+              ],
+            )),
+      ),
+    ));
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have account? ",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const SignUpScreen()));
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
